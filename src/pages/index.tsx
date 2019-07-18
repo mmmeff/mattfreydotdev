88d54cbc52a7b72c6037eace1039681ba0d9ee0e
mmmeff <matthewfrey118@gmail.com>
2019-07-18 08:13:54 -0700

    Fixing some responsive issues

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,21 +26,18 @@ export default class Home extends React.PureComponent {
                 <div className="glyph"></div>
                 
                 <div className="content">
-                    <div className="the-fold">
-                        <h1 className="content__name">
-                            Matt Frey
-                        </h1>
-                        <h3 className="content__is-a">
-                            is currently a <strong>
-                                Front-End Engineer
-                            </strong> at <strong className="imdb">
-                                <a href="https://www.imdb.com">IMDb</a>
-                            </strong> and lives in <strong>
-                                Seattle,&nbsp;WA&nbsp;<GiMountaintop />
-                            </strong>
-                        </h3>
-                        <div className="content__spacer" />
-                    </div>
+                    <h1 className="content__name">
+                        Matt Frey
+                    </h1>
+                    <h3 className="content__is-a">
+                        is currently a <strong>
+                            Front-End Engineer
+                        </strong> at <strong className="imdb">
+                            <a href="https://www.imdb.com">IMDb</a>
+                        </strong> and lives in <strong>
+                            Seattle,&nbsp;WA&nbsp;<GiMountaintop />
+                        </strong>
+                    </h3>
 
                     <div className="content__info">
                         {
@@ -65,34 +62,33 @@ export default class Home extends React.PureComponent {
 
 const StyledContainer = styled.div`
     .content {
-        position: relative;
-        max-width: 1024px;
+        display: grid;
+        grid-template-columns: 1fr;
+        grid-template-rows: 1fr 1fr 1fr 1fr 1fr;
+        height: 100vh;
         margin: 0 auto;
-        padding: 0 2rem 2rem;
+        max-width: 1024px;
+        overflow: hidden;
+        position: relative;
         z-index: 2;
 
-        .the-fold {
-            height: 100vh;
-            display: flex;
-            flex-direction: column;
-            justify-content: space-between;
-            overflow: hidden;
-            position: relative;
+        @media (min-width: 420px)  {
+            padding: 0 2rem 2rem;
         }
 
         strong {
             color: white;
-            text-shadow: 1px 2px red;
             font-style: italic;
             font-size: 1.1em;
+            text-shadow: 1px 2px red;
 
             &.imdb {
-                display: inline-block;
                 background: #F5C518;
-                padding: 0 0.33em;
                 border-radius: 5px;
+                display: inline-block;
                 font-family: 'Impact', sans-serif;
                 font-style: initial;
+                padding: 0 0.33em;
                 text-shadow: none;
 
                 a { 
@@ -103,62 +99,38 @@ const StyledContainer = styled.div`
         }
 
         &__name {
-            font-size: 3em;
+            font-size: 5rem;
+            grid-row: span 2;
+            margin: 0;
+            place-self: end;
             text-align: center;
             width: 100%;
-            margin: 0;
-            position: absolute;
-            top: 3rem;
 
             @media (min-width: 1024px) {
                 font-size: 10rem;
-                text-align: right;
-                width: 40vw;
-                top: 0;
-                left: 0;
             }
         }
 
         &__is-a {
             font-size: 2rem;
-            width: 100%;
+            grid-row: span 2;
+            place-self: center;
             text-align: center;
-            position: absolute;
-            bottom: 15vh;
+            width: 100%;
 
-            @media (min-width: 768px) {
+            @media (min-width: 1024px) {
                 font-size: 3rem;
-                text-align: left;
-                margin: 0 0 0 auto;
-                width: 50vw;
-                bottom: 10rem;
-                right: 0;
             }
         }
 
-        &__spacer {
-            height: 5rem;
-            width: 33%;
-            color: ${ ({ theme }) => theme.colors.lineColor };
-            opacity: 0.5;
-            margin: 3rem auto;
-            clip-path: polygon(0 0, 50% 33%, 100% 0, 100% 66%, 50% 100%, 0 66%);
-            background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAQAAAAECAYAAACp8Z5+AAAAEklEQVQImWNgYGD4z0AswK4SAFXuAf8EPy+xAAAAAElFTkSuQmCC);
-            flex-shrink: 0;
-            position: absolute;
-            bottom: 0;
-            left: 0;
-            right: 0;
-        }
-
         &__info {
-            border: ${ ({ theme }) => theme.lineSizes.thin } solid ${ ({ theme }) => theme.colors.lineColor };
-            border: ${ ({ theme }) => theme.lineSizes.thin } solid ${ ({ theme }) => theme.colors.primary };
-            border-radius: 3px;
-            padding: ${ ({ theme }) => theme.lineSizes.thic };
-            display: flex;
             align-items: center;
+            border-radius: 3px;
+            border: ${ ({ theme }) => theme.lineSizes.thin } solid ${ ({ theme }) => theme.colors.primary };
+            display: flex;
             justify-content: center;
+            padding: ${ ({ theme }) => theme.lineSizes.thic };
+            place-self: center;
 
             a {
                 color: white;
@@ -172,8 +144,6 @@ const StyledContainer = styled.div`
         clip-path: polygon(0% 0%, 0% 100%, 25% 100%, 25% 25%, 75% 25%, 75% 75%, 25% 75%, 25% 100%, 100% 100%, 100% 0%);
         width: 40vw;
         height: 40vw;
-        /* border-radius: 100%; */
-        /* background: black; */
         background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAQAAAAECAYAAACp8Z5+AAAAEklEQVQImWNgYGD4z0AswK4SAFXuAf8EPy+xAAAAAElFTkSuQmCC);
         margin: 5rem auto 0;
         mix-blend-mode: overlay;
