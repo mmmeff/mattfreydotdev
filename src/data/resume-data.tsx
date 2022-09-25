293d44cbe4be186f92f2ea0649aa55e9332f581b
Matthew Frey <matthewfrey118@gmail.com>
2022-09-25 08:09:33 +0000

    Update src/data/resume-data.tsx

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -25,11 +25,11 @@ export const experience: ExperienceItem[] = [
         startDate: '06/2022',
         endDate: 'present',
         responsibilities: [
-            'Leading the design and implementation of Fusion by STOKE, a SaaS product that solves inventory and work management in the hardware space. Think JIRA for hardware',
+            'Leading the design and implementation of Fusion by STOKE, a SaaS product that solves inventory tracking and work management in the hardware space. Think JIRA for the hardware space',
             'Full-stack development of the entire platform, including a Prisma-powered GraphQL API and a Next.js front-end',
             'Coaching and mentoring junior engineers on the team as we tackle an ambiguous problem space',
             'Designing, building, and maintaining infrastructure on AWS Fargate, CI/CD, end-to-end testing, and operational observability',
-            'Occasionally installing a network switch or two at our engine test facilities'
+            'Occasionally installing a network switch or two at our engine test facilities in the desert'
         ]
     },
     {
